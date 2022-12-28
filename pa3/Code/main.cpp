54fd0c13b8659d65bd394d9f33dc82e3276c17a1
LinkinPony <friendshipgame714@gmail.com>
2022-12-28 16:26:20 +0800

    finish phone shading

--- pa3/Code/main.cpp
+++ pa3/Code/main.cpp
@@ -167,7 +167,23 @@ Eigen::Vector3f phong_fragment_shader(const fragment_shader_payload& payload)
     {
         // TODO: For each light source in the code, calculate what the *ambient*, *diffuse*, and *specular* 
         // components are. Then, accumulate that result on the *result_color* object.
-        
+        auto I = light.intensity;
+        //Diffuse reflection
+        Vector3f n = normal.normalized();
+        Vector3f l = light.position - payload.view_pos;
+        float r = l.norm();
+        l = l.normalized();
+        Vector3f Ld = kd.cwiseProduct(I) / (r * r) * fmax(0.0,n.dot(l));
+
+        //Specular highlights
+        Vector3f v = (eye_pos - payload.view_pos).normalized();
+        Vector3f h = (v + l).normalized();
+        Vector3f Ls = ks.cwiseProduct(I) / (r * r) * pow(fmax(0.0,n.dot(h)),p);
+
+        //Ambient lighting
+        Vector3f La = ka.cwiseProduct(amb_light_intensity);
+
+        result_color += Ld + Ls + La;
     }
 
     return result_color * 255.f;
@@ -215,7 +231,6 @@ Eigen::Vector3f displacement_fragment_shader(const fragment_shader_payload& payl
     {
         // TODO: For each light source in the code, calculate what the *ambient*, *diffuse*, and *specular* 
         // components are. Then, accumulate that result on the *result_color* object.
-
 
     }
 
